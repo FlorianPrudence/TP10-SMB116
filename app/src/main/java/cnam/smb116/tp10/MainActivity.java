4e290a7a2cc54a94a08ba96a56f99b36bcd6d5f7
Florian PRUDENCE <florian.prudence@altazion.com>
2023-06-11 16:54:38 +0200

    Ajout de commentaires et de détails supplémentaires

--- app/src/main/java/cnam/smb116/tp10/MainActivity.java
+++ app/src/main/java/cnam/smb116/tp10/MainActivity.java
@@ -23,18 +23,24 @@ public class MainActivity extends AppCompatActivity {
     private NsdManager.ResolveListener resolveListener;
     private NsdManager mNsdManager;
     private String SERVICE_NAME = "SMB116";
-    private String SERVICE_TYPE = "_http._tcp.";
+    private final String SERVICE_TYPE = "_http._tcp.";
     private TextView tVServer, tVClient;
     private Button btnStartService, btnStopService, btnStartDiscover, btnStopDiscover, btnSendMessage, btnFinish;
+    private String serviceIP;
+    private int servicePort;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // On commence par créer les différents listener dont on aura besoin dans l'activité
         registrationListener = initializeRegistrationListener();
         resolveListener = initializeResolveListener();
         discoveryListener = initializeDiscoveryListener();
+
+        // On map les éléments de l'IHM sur les objets correspondants
         btnStartService = findViewById(R.id.button_start_service);
         btnStopService = findViewById(R.id.button_stop_service);
         btnStartDiscover = findViewById(R.id.button_start_discovering);
@@ -54,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
         btnStopService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
                     mNsdManager.unregisterService(registrationListener);
             }
         });
-
         btnStartDiscover.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,7 +75,6 @@ public class MainActivity extends AppCompatActivity {
                 tVClient.append("Discovering started : " + SERVICE_TYPE + "\n");
             }
         });
-
         btnStopDiscover.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,7 +83,13 @@ public class MainActivity extends AppCompatActivity {
                 tVClient.append("Discovery stopped : " + SERVICE_TYPE + "\n");
             }
         });
-
+        btnSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ici se trouve le code pour envoyer un message en utilisant l'IP du service
+                // et son port récupérés via la méthode onServiceResolved().
+            }
+        });
         btnFinish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -96,9 +105,8 @@ public class MainActivity extends AppCompatActivity {
             mNsdManager = (NsdManager) getSystemService(Context.NSD_SERVICE);
     }
 
+    // Méthode permettant d'initialiser le Service
     public void initializeServer() throws IOException {
-
-
         // Initialize a server socket on the next available port.
         ServerSocket serverSocket = new ServerSocket(0);
         // Get the chosen port.
@@ -113,9 +121,9 @@ public class MainActivity extends AppCompatActivity {
                 serviceInfo, NsdManager.PROTOCOL_DNS_SD, registrationListener);
     }
 
+    // Permet de gérer le comportement du service tout au long de son cycle de vie
     public NsdManager.RegistrationListener initializeRegistrationListener() {
         return new NsdManager.RegistrationListener() {
-
             @Override
             public void onServiceRegistered(NsdServiceInfo serviceInfo) {
                 // Save the service name. Android may have changed it in order to
@@ -130,14 +138,7 @@ public class MainActivity extends AppCompatActivity {
                         btnStopService.setEnabled(true);
                     }
                 });
-
             }
-
-            @Override
-            public void onRegistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
-                // Registration failed! Put debugging code here to determine why.
-            }
-
             @Override
             public void onServiceUnregistered(NsdServiceInfo arg0) {
                 runOnUiThread(new Runnable() {
@@ -146,11 +147,14 @@ public class MainActivity extends AppCompatActivity {
                         tVServer.append("onServiceUnregistered\n");
                         btnStartService.setEnabled(true);
                         btnStopService.setEnabled(false);
+                        btnSendMessage.setEnabled(false);
                     }
                 });
-
             }
-
+            @Override
+            public void onRegistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                // Registration failed! Put debugging code here to determine why.
+            }
             @Override
             public void onUnregistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
                 // Unregistration failed. Put debugging code here to determine why.
@@ -159,10 +163,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public NsdManager.DiscoveryListener initializeDiscoveryListener() {
-
         // Instantiate a new DiscoveryListener
         return new NsdManager.DiscoveryListener() {
-
             // Called as soon as service discovery begins.
             @Override
             public void onDiscoveryStarted(String regType) {
@@ -175,7 +177,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
             }
-
             @Override
             public void onServiceFound(NsdServiceInfo service) {
                 runOnUiThread(new Runnable() {
@@ -188,7 +189,6 @@ public class MainActivity extends AppCompatActivity {
                     mNsdManager.resolveService(service, resolveListener);
                 }
             }
-
             @Override
             public void onServiceLost(NsdServiceInfo service) {
                 runOnUiThread(new Runnable() {
@@ -198,7 +198,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
             }
-
             @Override
             public void onDiscoveryStopped(String serviceType) {
                 runOnUiThread(new Runnable() {
@@ -210,7 +209,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
             }
-
             @Override
             public void onStartDiscoveryFailed(String serviceType, int errorCode) {
                 runOnUiThread(new Runnable() {
@@ -221,7 +219,6 @@ public class MainActivity extends AppCompatActivity {
                 });
                 mNsdManager.stopServiceDiscovery(this);
             }
-
             @Override
             public void onStopDiscoveryFailed(String serviceType, int errorCode) {
                 runOnUiThread(new Runnable() {
@@ -237,21 +234,20 @@ public class MainActivity extends AppCompatActivity {
 
     public NsdManager.ResolveListener initializeResolveListener() {
         return new NsdManager.ResolveListener() {
-
             @Override
             public void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode) {
             }
-
             @Override
             public void onServiceResolved(NsdServiceInfo serviceInfo) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         String serviceName = serviceInfo.getServiceName();
-                        int port = serviceInfo.getPort();
-                        String hostIp = serviceInfo.getHost().getHostAddress();
+                        servicePort = serviceInfo.getPort();
+                        serviceIP = serviceInfo.getHost().getHostAddress();
 
-                        tVClient.append("onServiceResolved: " + serviceName + "(port: " + port + ", adresse: " + hostIp + ")\n");
+                        tVClient.append("onServiceResolved: " + serviceName + "(port: " + servicePort + ", adresse: " + serviceIP + ")\n");
+                        btnSendMessage.setEnabled(true);
                     }
                 });
             }
